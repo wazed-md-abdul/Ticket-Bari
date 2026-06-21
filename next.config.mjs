813b35ba0d9c00cff7ffebfedc8a56381a181e90
Md Abdul Wazed <wazedjiad@gmail.com>
2026-06-21 16:44:46 +0800

    chore: update next.config.mjs to support Google profile, Unsplash, and ImgBB remote images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "i.ibb.co" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+    ],
+  },
 };
 
 export default nextConfig;
